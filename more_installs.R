8c396c6a2de0071d40b4c1d08b9a04b717eb3ceb
fomightez <wayne4.antispam@server.fake>
2021-07-01 16:36:18 -0400

    add imagemagick

--- more_installs.R
+++ more_installs.R
@@ -2,8 +2,11 @@
 # without them R complained
 install.packages("datelife", repos='http://cran.us.r-project.org') # based on https://github.com/phylotastic/datelife
 install.packages("rphylopic", repos='http://cran.us.r-project.org') # based on https://github.com/sckott/rphylopic
-devtools::install_github("galacticpolymath/galacticEdTools")
 # I suppose 'Remotes:' at bottom https://github.com/galacticpolymath/galacticEdTools/blob/main/DESCRIPTION means
 # use of `remotes` like at https://github.com/fomightez/rayshader-binder/blob/master/install.R for Rayshader
 # and given fact https://github.com/galacticpolymath/galacticEdTools/blob/main/DESCRIPTION listed as a package remotes
 remotes::install_github("phylotastic/datelife")
+remotes::install_bioc("ggtree")
+
+# install from github since not on CRAN yet, as directed at https://galacticpolymath.github.io/galacticEdTools/
+devtools::install_github("galacticpolymath/galacticEdTools")

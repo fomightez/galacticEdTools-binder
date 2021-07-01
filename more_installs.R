7083d9b7c61f191ebc5b51871b3e76d014bf034e
fomightez <wayne4.antispam@server.fake>
2021-07-01 16:29:16 -0400

    start binderizing of galacticEdTools

---
+++ more_installs.R
@@ -0,0 +1,9 @@
+# Specifying the mirrors is based on https://stackoverflow.com/a/11488224/8508004 and was added because 
+# without them R complained
+install.packages("datelife", repos='http://cran.us.r-project.org') # based on https://github.com/phylotastic/datelife
+install.packages("rphylopic", repos='http://cran.us.r-project.org') # based on https://github.com/sckott/rphylopic
+devtools::install_github("galacticpolymath/galacticEdTools")
+# I suppose 'Remotes:' at bottom https://github.com/galacticpolymath/galacticEdTools/blob/main/DESCRIPTION means
+# use of `remotes` like at https://github.com/fomightez/rayshader-binder/blob/master/install.R for Rayshader
+# and given fact https://github.com/galacticpolymath/galacticEdTools/blob/main/DESCRIPTION listed as a package remotes
+remotes::install_github("phylotastic/datelife")

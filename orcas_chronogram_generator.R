4abf5ddaadb8e8eafd70aa073a0346b8f9738232
fomightez <wayne4.antispam@server.fake>
2021-07-02 13:17:40 -0400

    add demo files

---
+++ orcas_chronogram_generator.R
@@ -0,0 +1,4 @@
+library(galacticEdTools)
+# Illustrates "how orcas are really dolphins, & are deeply diverged from other whales."- Matt Wilkins
+# SOURCE: https://twitter.com/mattwilkinsbio/status/1410013532855443456
+showPhylo(c("blue whale","humpback whale","sperm whale","orca","narwhal","bottle nosed dolphin"),"c")
